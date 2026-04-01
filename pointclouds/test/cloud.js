c2856b74189d86941a922525a7e67fbb37fc3447
seshat-saito <saito@seshat.co.jp>
2026-04-01 10:58:36 +0900

    input

---
+++ pointclouds/test/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 20000000,
+    "boundingBox": {
+        "lx": -30.988000000000004,
+        "ly": -177.87400000000003,
+        "lz": -1.969,
+        "ux": 153.42600000000003,
+        "uy": 6.539999999999992,
+        "uz": 182.44500000000003
+    },
+    "tightBoundingBox": {
+        "lx": -30.927000000000004,
+        "ly": -177.861,
+        "lz": -1.8990000000000003,
+        "ux": 116.935,
+        "uy": 6.2790000000000039,
+        "uz": 9.889
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.05000000074505806,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
